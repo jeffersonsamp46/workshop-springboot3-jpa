f2f58c39d8946c62215d2fe45f2ab36e723a9a96
jeffersonsamp46 <jeffersonsamp@gmail.com>
2024-08-28 16:29:48 -0300

    Service layer, component registration

--- src/main/java/com/sampaio/course/resources/UserResource.java
+++ src/main/java/com/sampaio/course/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.sampaio.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sampaio.course.entities.User;
+import com.sampaio.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(1L, "Jefferson Sampaio", "jeffersonsamp@gmail.com", "98988883256", "123456");
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id);
 		
 		return ResponseEntity.ok().body(user);
 	}
