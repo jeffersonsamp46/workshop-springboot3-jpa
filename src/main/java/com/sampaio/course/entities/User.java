0a8860069b715f36184dce4a7db81023b840b383
jeffersonsamp46 <jeffersonsamp@gmail.com>
2024-08-28 15:34:03 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/sampaio/course/entities/User.java
+++ src/main/java/com/sampaio/course/entities/User.java
@@ -17,7 +17,7 @@ public class User implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
@@ -26,7 +26,7 @@ public class User implements Serializable {
 	public User() {
 	}
 	
-	public User(long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -35,11 +35,11 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -77,7 +77,7 @@ public class User implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(email, id, name, password, phone);
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -89,8 +89,8 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(email, other.email) && id == other.id && Objects.equals(name, other.name)
-				&& Objects.equals(password, other.password) && Objects.equals(phone, other.phone);
+		return Objects.equals(id, other.id);
 	}
+	
 	
 }
---
+++ src/main/java/com/sampaio/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sampaio.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sampaio.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
